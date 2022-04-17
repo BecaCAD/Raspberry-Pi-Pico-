4b240563e34ad19c72d8f47ba75e72af1ea1a6e4
Phil <45577042+BecaCAD@users.noreply.github.com>
2022-04-17 22:09:57 +1200

    Update main.py
    Revised M value (line 53) and the RTC setting takes 7 values. The Weekday value is 1-7 as a valid option, new comment line 35). Edited line 36 to reflect the seven items of setting the date time.

--- DS1302 RTC/main.py
+++ DS1302 RTC/main.py
@@ -32,7 +32,8 @@ ds = DS1302(Pin(18),Pin(17),Pin(16))
 
 ds.date_time() # returns the current datetime.
 
-#ds.date_time([2023, 3, 2, 0, 8, 17, 50, 0]) # set datetime.
+# set YYYY, MM, DD, Week day (1-7), Hr, Min, Sec
+#ds.date_time([2023, 3, 2, 1, 8, 17, 50]) # set datetime.
 
 #ds.hour() # returns hour.
 #print(ds.date_time())
@@ -49,7 +50,7 @@ while True:
     if D < 10:
         D = "0" + str(D)
     if M < 10:
-        hr = "0" + str(M)
+        M = "0" + str(M)
         
     lcd.move_to(0,0)
     lcd.putstr("Time:")
